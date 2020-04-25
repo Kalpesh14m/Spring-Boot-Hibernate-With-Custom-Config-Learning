8eae3c89a0b11f570fbe55011d4115aec12bc205
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-04-26 05:28:58 +0530

    Refactor

--- src/main/java/com/playground/Playground2.java
+++
@@ -1,8 +0,0 @@
-package com.playground;
-
-public class Playground2 {
-	public static void main(String[] args) {
-		String query = "select * from user where name = 'kalpesh' ;";
-		System.out.println(query);
-	}
-}
